a3595a9aad919c2f8fdbf7ef4bf0807785952159
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-01 17:50:34 +0200

    added simple counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      someKey: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({count: this.state.count + 1});
+
+    console.log('from handle click');
+    // this.setState ((prevState)=>({count: prevState.count + 1}));
+  }
+
   render() {
-    return <div>Hello from React</div>
+    return (
+    <div className="App">
+      Hello from React
+      <button onClick={this.handleClick}>{this.state.count}</button>
+    </div>
+    );
   }
 }
 
