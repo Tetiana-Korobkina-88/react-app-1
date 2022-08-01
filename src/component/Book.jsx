92d9f57c6de299d1b23b1d7023f686b3a0dd03a2
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-01 15:53:48 +0200

    working with conditional rendering

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -1,8 +1,10 @@
 export const Book = (props) => {
-  return <div>
-    <h1>Today is {props.name ? <span>{props.name}</span> : 'Ooops'}</h1>
+  return props.name ? (
+  <div>
+    <h1>Today is {props.name}</h1>
     <p>{props.year}</p>
     <p>{props.price}</p>
     <p>{props.children}</p>
   </div>
-}
+  ) : null
+};
