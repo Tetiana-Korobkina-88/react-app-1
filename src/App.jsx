863d6e1865b3d1a22758028c453554dd4f4cab92
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-01 16:18:32 +0200

    added Preloader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
 import { Book } from './component/Book';
+import { Preloader } from './component/Preloader';
 
-const App = () => {
-  return <div>
+const App = (props) => {
+  return props.isLoading ?  (<Preloader />) : <div>
     <Book name='Monday' year='2022' price='2500'>it's usually a hart day</Book>
     <Book name='Tuesday' year='2021' price='3500'/>
     <Book name='Wednesday' year='2020' price='6500'/>
---
+++ src/component/Preloader.jsx
@@ -0,0 +1,3 @@
+export const Preloader = () => {
+  return <h3>loading in progress</h3>
+}
